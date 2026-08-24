ca406981c099819518b7b114d02228ea43d3e789
Akshatsoni27 <akshatsoni0427@gmail.com>
2026-08-24 19:42:39 +0530

    DB

--- create-db.py
+++ create-db.py
@@ -1,6 +1,31 @@
-'''
-load pdf
-split into chunks
-create embeddings
-store into chroma db
-'''
+#load pdf 
+#split into chunks 
+#create the embeddings 
+#store into chroma 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import Chroma 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = PyPDFLoader("document_Loaders/deeplearning.pdf")
+docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs)
+
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-001"
+)
+
+vectorstore = Chroma.from_documents(
+    documents= chunks,
+    embedding=embedding_model,
+    persist_directory="chroma_db"
+)
